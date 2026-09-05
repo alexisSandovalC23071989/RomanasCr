00ce05b96d7749f511676644a9eae9e28ff5ae09
Alexis <aldavid23@gmail.com>
2026-09-04 22:30:21 -0600

    Arquitentura Inicial

---
+++ Abstraciones/Interfaces/IUsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Abstraciones.Modelos.ModeloAutenticacion;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abstraciones.Interfaces
+{
+    public  interface IUsuarioController
+    {
+        Task<IActionResult> Obtener();
+        Task<IActionResult> Obtener(Guid id);
+
+        Task<IActionResult> Agregar(UsuarioResponse usuario);
+        Task<IActionResult>Editar(Guid id, UsuarioResponse usuario);
+
+        Task<IActionResult>Eliminar(Guid id);
+
+    }
+}
---
+++ Abstraciones/Modelos/ModeloAutenticacion/UsuarioResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstraciones.Modelos.ModeloAutenticacion
+{
+    public class UsuarioResponse
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }  = string.Empty;
+        public string Correo { get; set; }= string.Empty;
+
+        public string Telefono { get; set; }= string.Empty;
+
+
+    }
+}
